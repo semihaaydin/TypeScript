b13ac8f01e426366e8c246233b33b8d4c594b5ff
Semiha Aydın <semiha.aydinn@gmail.com>
2022-12-07 00:29:01 +0300

    add new Kata
    Co-Authored-By: Semiha Aydın <29448582+semihaaydin@users.noreply.github.com>

---
+++ Algorithm/ReadME.txt
@@ -0,0 +1,1 @@
+commmenerelriferşsc
---
+++ Algorithm/squareSum.ts
@@ -0,0 +1,12 @@
+/*Complete the square sum function so that it squares 
+each number passed into it and then sums the results together.
+For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9. */
+
+console.log("Test algorithm Problems SquareSum from Codewars");
+function squareSum(numbers: number[]): number {
+    /** let total=0;
+    for(let item of numbers){total+=item*item;}
+    return total; */
+    return numbers.reduce((prev, curr) => prev + curr * curr, 0);
+}
+console.log(squareSum([0,3,4,5]));
